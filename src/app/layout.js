770fc46ee279c6b690879b10686f91bab28813b9
Rohit Bajaj <108188800+itsRohit47@users.noreply.github.com>
2024-04-23 16:04:26 +1000

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,13 +11,7 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
-  window.onUsersnapLoad = function(api) {
-    api.init();
-  };
-  var script = document.createElement('script');
-  script.defer = 1;
-  script.src = 'https://widget.usersnap.com/global/load/6ead6e02-9f4d-438c-974a-8445829c47aa?onload=onUsersnapLoad';
-  document.getElementsByTagName('head')[0].appendChild(script);
+  
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
